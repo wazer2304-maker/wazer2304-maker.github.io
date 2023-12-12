3e938fefd3f8d548e06319e8726ce10a743c5620
Anton Arningsmark <79598587+wazer2304-maker@users.noreply.github.com>
2023-12-12 11:26:34 +0100

    Labb - added features

--- js/labb.js
+++ js/labb.js
@@ -9,6 +9,7 @@ Object.defineProperty(String.prototype, 'cap', {
 function a() {
  l = document.getElementById('la');
     const inputs = document.querySelectorAll('input');
+    const textareas = document.querySelectorAll('textarea');
     let text = '';
 
 
@@ -43,91 +44,91 @@ function a() {
     
     text += "<br><br><br><h1>Inledning</h1>\n";
     
-    if(!(inputs[7].value == "")) {
+    if(!(textareas[0].value == "")) {
     text += "<h2>Bakgrund</h2>\n";
-    text += inputs[7].value;
+    text += textareas[0].value;
     }
-    if(!(inputs[8].value == "")){
+    if(!(textareas[1].value == "")){
     text += "<h2>Syfte</h2>\n";
-    text += inputs[8].value;
+    text += textareas[1].value;
 }
-    if(!(inputs[9].value == "")){
+    if(!(textareas[2].value == "")){
     text += "<h2>Frågeställning</h2>\n";
-    text += inputs[9].value;
+    text += textareas[2].value;
 }
-    if(!(inputs[10].value == "")){
-    text += "<br> " + inputs[10].value;
+    if(!(textareas[3].value == "")){
+    text += "<br> " + textareas[3].value;
 }
     
     
     
     text += "<br><br><br><h1>Metod</h1>\n";
     
-    if(!(inputs[11].value == "")){
+    if(!(textareas[4].value == "")){
     text += "<h2>Materiel </h2>\n";
-    text += inputs[11].value;
+    text += textareas[4].value;
 }
-    if(!(inputs[12].value == "")){
+    if(!(textareas[5].value == "")){
     text += "<h2>Kemikalier</h2>\n";
-    text += inputs[12].value;
+    text += textareas[5].value;
 }
-    if(!(inputs[13].value == "")){
+    if(!(textareas[6].value == "")){
     text += "<h2>Genomförande</h2>\n";
-    text += inputs[13].value;   
+    text += textareas[6].value;   
 }
     
     text += "<br><br><br><h1>Resultat</h1>\n";
     
-    if(!(inputs[14].value == "")){
+    if(!(textareas[7].value == "")){
     text += "<h2>Tabell</h2>\n";
-    text += inputs[14].value;
+    text += textareas[7].value;
 }
     
-    if(!(inputs[15].value == "")){
+    if(!(textareas[8].value == "")){
     text += "<h2>Diagram</h2>\n";
-    text += inputs[15].value;
+    text += textareas[8].value;
 }
     
-    if(!(inputs[16].value == "")){
+    if(!(textareas[9].value == "")){
     text += "<h2>Beräkningar och Resultat</h2>\n";
-    text += inputs[16].value;
+    text += textareas[9].value;
 }
     
     text += "<br><br><br><h1>Diskussion</h1>\n";
 
-    if(!(inputs[17].value == "")){
+    if(!(textareas[10].value == "")){
     text += "<h2>Besvarande</h2>\n";
-    text += inputs[17].value;
+    text += textareas[10].value;
 }
     
-    if(!(inputs[18].value == "")){
+    if(!(textareas[11].value == "")){
     text += "<h2>Felkälla (1)</h2>\n";
-    text += inputs[18].value;
+    text += textareas[11].value;
 }
     
-    if(!(inputs[19].value == "")){
+    if(!(textareas[12].value == "")){
     text += "<h2>Felkälla (2)</h2>\n";
-    text += inputs[19].value;
+    text += textareas[12].value;
 }
-    if(!(inputs[20].value == "")){
+    if(!(textareas[13].value == "")){
     text += "<h2>Felkälla (3)</h2>\n";
-    text += inputs[20].value;
+    text += textareas[13].value;
 }
-    if(!(inputs[21].value == "")){
+    if(!(textareas[14].value == "")){
     text += "<h2>Resultatets trovärdighet</h2>\n";
-    text += inputs[21].value;
+    text += textareas[14].value;
 }
-    if(!(inputs[22].value == "")){
+    if(!(textareas[15].value == "")){
     text += "<h2>Vad som kan förbättras</h2>\n";
-    text += inputs[22].value;
+    text += textareas[15].value;
 }
-    if(!(inputs[23].value == "")){
+    if(!(textareas[16].value == "")){
     text += "<h2>Sammanfattning</h2>\n";
-    text += inputs[23].value;
+    text += textareas[16].value;
 }
-    if(!(inputs[24].value == "")){
+    if(!(textareas[17].value == "")){
     text += "<br><br><br><h1>Referenser & Källor</h1>\n"; 
-    text += inputs[24].value;
+    text += textareas[17].value;
 }
     
     l.innerHTML = text;
